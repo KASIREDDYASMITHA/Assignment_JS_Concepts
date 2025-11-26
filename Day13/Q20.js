9fc2725462aff00fbfb0b89f0f5d93998fbd98c1
KASIREDDYASMITHA <asmithareddy2022@gmail.com>
2025-11-26 09:14:34 +0530

    all files commit

--- Day13/Q20.js
+++ Day13/Q20.js
@@ -1,6 +1,5 @@
 function createCounter() {
-    let count = 0; // private variable
-
+    let count = 0; 
     return {
         increment: function () {
             count++;
@@ -16,21 +15,21 @@ function createCounter() {
     };
 }
 
-// Example Usage
+
 const counter1 = createCounter();
-counter1.increment(); // Current count: 1
-counter1.increment(); // Current count: 2
-counter1.decrement(); // Current count: 1
+counter1.increment(); 
+counter1.increment(); 
+counter1.decrement();
 
 const counter2 = createCounter();
-counter2.increment(); // Current count: 1 (separate counter)
+counter2.increment();
 
 
 
 //q2
 function createBankAccount() {
-    let balance = 0; // private variable
-    let transactionHistory = []; // private transaction log
+    let balance = 0; 
+    let transactionHistory = []; 
 
     return {
         deposit: function (amount) {
@@ -57,12 +56,12 @@ function createBankAccount() {
     };
 }
 
-// Example Usage
-const account = createBankAccount();
-account.deposit(500);   // Deposited: 500
-account.withdraw(200);  // Withdrawn: 200
-account.withdraw(400);  // Insufficient balance
-account.checkBalance(); // Current balance: 300
-account.getHistory();   // Transaction History: [ 'Deposited: 500', 'Withdrawn: 200', 'Failed withdrawal: 400' ]
 
-console.log(account.balance); // undefined (cannot access directly)
+const account = createBankAccount();
+account.deposit(500);   
+account.withdraw(200); 
+account.withdraw(400); 
+account.checkBalance(); 
+account.getHistory();   
+
+console.log(account.balance); 
